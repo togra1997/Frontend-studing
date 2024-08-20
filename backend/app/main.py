f14e7489ff5c0b38e8f2141eaed2bcaa4e527fed
togra_mac <ryoota1997@gmail.com>
2024-08-21 01:18:31 +0900

    npxのserveで動くように変更

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,8 +21,8 @@ class Task(BaseModel):
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
